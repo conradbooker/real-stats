bf55eec6c683af423710ba5d8bd78f01b689344b
conradbooker <conrad.booker@gmail.com>
2023-02-08 16:19:48 -0500

    Initial Commit

---
+++ real-stats/real_statsApp.swift
@@ -0,0 +1,17 @@
+//
+//  real_statsApp.swift
+//  real-stats
+//
+//  Created by Conrad on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct real_statsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
